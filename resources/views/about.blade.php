457dd5bd5adfca0f05559d71e5a08c11130ce72a
nandan1475 <nandankumar1475@gmail.com>
2024-02-17 18:18:21 +0530

    first project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About page </title>
+</head>
+<body>
+    
+    @include('pages.header')
+    <h1>About page</h1>
+    <div class="news">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi eligendi nam ut aliquam eos nobis sequi impedit excepturi officiis fuga accusamus laboriosam reprehenderit, recusandae porro ullam eum, illo libero aut? Doloremque consequatur eum quasi nobis molestiae quo! Nobis officiis, tempora reiciendis distinctio voluptatibus deserunt aspernatur tempore dignissimos. Dignissimos excepturi autem ducimus doloribus minus iusto officia molestiae id distinctio nihil, totam pariatur voluptates ea ipsam, placeat ab recusandae consequuntur, architecto reprehenderit quibusdam est nemo praesentium possimus? Hic veniam, quis recusandae odio natus nisi debitis ducimus eius deserunt quaerat doloribus. Veniam ducimus illum eveniet praesentium aut. Officia, placeat. Minus vitae esse eveniet.
+    </div>
+    @include('pages.footer')
+</body>
+</html>
